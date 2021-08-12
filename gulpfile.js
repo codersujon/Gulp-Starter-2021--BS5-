e9ccf46d17ec06494ab15e756429fdee38706c92
MD.SUJAN AHMED <csesujon155@gmail.com>
2021-08-12 06:51:11 +0600

    gulp-fileInclude and Use

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ let gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     browserify = require('browserify'),
     rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
+    fileinclude = require('gulp-file-include');
+notify = require('gulp-notify'),
     options = require('gulp-options'),
     gulpIf = require('gulp-if'),
     babelify = require('babelify'),
@@ -20,7 +21,7 @@ let gulp = require('gulp'),
 
 /*PATH*/
 const path = {
-    dist:{
+    dist: {
         htmlUrl: './dist',
         styleUrl: './dist/css/',
         imageUrl: './dist/img',
@@ -29,21 +30,23 @@ const path = {
         fontAwesomeUrl: './src/webfonts/',
         mapUrl: './'
     },
-    src:{
+    src: {
         htmlSrc: './src/**/*.html',
         styleSrc: 'src/scss/main.scss',
         jsSrc: './src/script/',
         fontAwesomeSrc: 'node_modules/@fortawesome/fontawesome-free/webfonts/*.*',
         fontsSrc: './src/webfonts/*.*',
-        imageSrc: './src/images/**/*.*'
+        imageSrc: './src/images/**/*.*',
+        partials: './src/partials/'
     },
-    watch:{
+    watch: {
         htmlWatch: './src/**/*.html',
         styleWatch: './src/scss/**/*.scss',
         jsWatch: './src/script/**/*.js',
         imgWatch: './src/images/**/*.*',
         fontsWatch: './src/fonts/**/*.*',
-        fontAwesomeWatch: './src/webfonts/**/*.*'
+        fontAwesomeWatch: './src/webfonts/**/*.*',
+        partials: './src/partials/**/*.*'
     }
 };
 
@@ -68,8 +71,16 @@ function reload(done) {
 /*TASKS*/
 
 //COMPILE HTML 
-function html() {
-    return triggerPlumber(path.src.htmlSrc, path.dist.htmlUrl);
+function html(done) {
+    gulp.src(path.src.htmlSrc)
+        .pipe(plumber())
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: path.src.partials
+        }))
+        .pipe(gulp.dest(path.dist.htmlUrl))
+        .pipe(browserSync.stream());
+    done();
 }
 
 
@@ -157,7 +168,7 @@ gulp.task('css', css);
 gulp.task('js', js)
 gulp.task('images', images)
 gulp.task('fontawesome', fontawesome),
-gulp.task('fonts', fonts)
+    gulp.task('fonts', fonts)
 
 gulp.task('default', gulp.parallel(html, css, js, images, fontawesome, fonts));
 gulp.task('watch', gulp.parallel(browser_sync, watch_files));
